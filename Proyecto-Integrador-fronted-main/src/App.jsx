88738a6777279efcd2a2bf7aee0a330b1a89a11f
ingenieropingui <146021049+ingenieropingui@users.noreply.github.com>
2025-10-09 21:43:41 -0500

    Update App.jsx

--- Proyecto-Integrador-fronted-main/src/App.jsx
+++ Proyecto-Integrador-fronted-main/src/App.jsx
@@ -5,37 +5,35 @@ import Home from './pages/Home';
 import MiPerfil from './pages/MiPerfil';
 import MisCursos from './pages/MisCursos';
 import MisFavoritos from './pages/MisFavoritos';
-import Register from "./pages/Register";
-import Login from './pages/Login'; 
+import Register from './pages/Register';
+import Login from './pages/Login';
 import Contact from './pages/Contact';
-import About from './pages/about';     
+import About from './pages/about';
 import Principal from './pages/principal';
 import './styles/App.css';
 
-
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Header />
-                <main className="main-content">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/mi-perfil" element={<MiPerfil />} />
-                        <Route path="/mis-cursos" element={<MisCursos />} />
-                        <Route path="/mis-favoritos" element={<MisFavoritos />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="/principal" element={<Principal />} />  
-                         <Route path="/perfil" element={<MiPerfil />} />
-
-                    </Routes>
-                </main>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mi-perfil" element={<MiPerfil />} />
+            <Route path="/mis-cursos" element={<MisCursos />} />
+            <Route path="/mis-favoritos" element={<MisFavoritos />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/principal" element={<Principal />} />
+            <Route path="/perfil" element={<MiPerfil />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
